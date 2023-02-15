3febb64c25f6487a06f62638c97fc04427281e9a
infinite-dev22 <mwigojm@gmail.com>
2023-02-15 21:34:51 +0300

    Adding Django Template Inheritance 	modified:   dashboard/templates/dashboard/base.html 	modified:   dashboard/templates/dashboard/blank-page.html 	modified:   dashboard/templates/dashboard/error-404.html 	modified:   dashboard/templates/dashboard/error-500.html 	modified:   dashboard/templates/dashboard/partials/navbar.html 	new file:   requirements.txt

--- admin_panel/views/admin_chats.py
+++ admin_panel/views/admin_chats.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models.functions import Coalesce
 from django.shortcuts import render, redirect
 
 from accounts.models import BaseUser
@@ -21,7 +22,7 @@ def index_users(request):
                 "users": None
             }
             return render(request, "admin_panel/chats/index_chats.html", context)
-    return redirect('login_admin_user')
+    return redirect("login_admin_user")
 
 
 # Permissions Views
@@ -35,14 +36,14 @@ def index_chats(request):
                 "chats": chats,
                 "users": users
             }
-            return render(request, "admin_panel/chats/index_chats.html", context)
+            return render(request, "admin_panel/chats/chat_detail.html", context)
         except ObjectDoesNotExist:
             context = {
                 "chat": None,
                 "users": None
             }
-            return render(request, "admin_panel/chats/index_chats.html", context)
-    return redirect('login_admin_user')
+            return render(request, "admin_panel/chats/chat_detail.html", context)
+    return redirect("login_admin_user")
 
 
 def create_chat(request):
@@ -65,5 +66,41 @@ def create_chat(request):
         context = {
             "organisations": organisations
         }
-        return render(request, "admin_panel/chats/edit_chat.html", context)
-    return redirect('login_admin_user')
+        return render(request, "admin_panel/chats/chat_detail.html", context)
+    return redirect("login_admin_user")
+
+
+def chat_details(request, pk):
+    if request.user.is_authenticated and request.user.is_superuser:
+        user = request.user
+        sender = BaseUser.objects.get(id=pk)
+        users = BaseUser.objects.all()
+        chats = ChatMessages.objects \
+            .order_by("created_on", "msg_sender", "msg_receiver") \
+            .distinct()
+        chat = ChatMessages.objects.all().order_by(Coalesce("created_on", "seen").asc())
+        unread = chats.filter(msg_sender=sender, msg_receiver=user, seen=False)
+        unread.update(seen=True) if unread.first() and unread.first().msg_receiver == user else ...
+
+        if request.method == "POST":
+            message = request.POST["message"]
+            msg_sender = user
+            receiver = request.POST["receiver"]
+            msg_receiver = BaseUser.objects.get(id=receiver)
+            ChatMessages(
+                body=message,
+                msg_sender=msg_sender,
+                msg_receiver=msg_receiver
+            ).save()
+            return redirect("chat_details", pk=pk)
+
+        context = {
+            "receiver": user,
+            "sender": sender,
+            "chats": chats,
+            "chat": chat,
+            "num": unread.count(),
+            "users": users
+        }
+        return render(request, "admin_panel/chats/chat_detail.html", context)
+    return redirect("login_admin_user")
